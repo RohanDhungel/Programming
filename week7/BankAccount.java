9ed65b8d10ab2d73ee2a4326ffb89efe0f868166
Rohan Dhungel <rohandhungel.work@gmail.com>
2026-04-19 13:29:40 +0545

    Update BankAccount.java

--- week7/BankAccount.java
+++ week7/BankAccount.java
@@ -1,52 +1,48 @@
 class BankAccount {
-
     int accountNumber;
-    String holderName;
-    double balance;
+    String accountHolder;
+    int balance;
 
-    BankAccount(int accountNumber, String holderName, double balance) {
+    BankAccount(int accountNumber, String accountHolder, int balance) {
         this.accountNumber = accountNumber;
-        this.holderName = holderName;
+        this.accountHolder = accountHolder;
         this.balance = balance;
     }
 
-    void deposit(double amount) {
+    void deposit(int amount) {
         balance = balance + amount;
-        System.out.println(holderName + " deposited: $" + amount);
+        System.out.println("Deposited: " + amount);
     }
 
-    void withdraw(double amount) {
-        if (amount > balance) {
-            System.out.println("Not enough balance for " + holderName);
-        } else {
+    void withdraw(int amount) {
+        if (amount > balance)
+            System.out.println("Not enough balance!");
+        else {
             balance = balance - amount;
-            System.out.println(holderName + " withdrew: $" + amount);
+            System.out.println("Withdrawn: " + amount);
         }
     }
 
-    void displayBalance() {
-        System.out.println("Account No : " + accountNumber);
-        System.out.println("Name       : " + holderName);
-        System.out.println("Balance    : $" + balance);
-        System.out.println("-------------------");
+    void display() {
+        System.out.println("Account No: " + accountNumber);
+        System.out.println("Holder: " + accountHolder);
+        System.out.println("Balance: " + balance);
+        System.out.println();
     }
 }
 
-public class MainClass {
-
+class MainClass {
     public static void main(String[] args) {
 
-        BankAccount account1 = new BankAccount(1001, "Alice", 5000);
-        BankAccount account2 = new BankAccount(1002, "Bob", 3000);
+        BankAccount b1 = new BankAccount(101, "Ali", 5000);
+        BankAccount b2 = new BankAccount(102, "Sara", 3000);
 
-        account1.deposit(1000);
-        account1.withdraw(500);
+        b1.display();
+        b1.deposit(1000);
+        b1.withdraw(500);
+        b1.display();
 
-        account2.deposit(700);
-        account2.withdraw(4000);  
-
-        System.out.println("\n--- Final Balance ---");
-        account1.displayBalance();
-        account2.displayBalance();
+        b2.display();
+        b2.withdraw(5000);
     }
 }
